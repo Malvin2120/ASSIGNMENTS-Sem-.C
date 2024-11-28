0eab44a8fbb09ddf4c44a357fccfad56fb917aa7
Malvin2120 <Malvinekings@gmail.com>
2024-11-28 20:35:08 +0300

    Create HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,16 @@
+// Program to print Hello World - Single Line comment
+/*
+Multi-line comment:
+Program to print Hello World
+Author : Malvine
+Reg No : ED102/G/20691/23
+Date    : 20/9/2024
+*/
+
+// Preprocessor directive for scanf() and printf()
+#include <stdio.h> 
+
+int main() {
+    printf("Hello World"); // Prints Hello World
+    return 0;
+}
